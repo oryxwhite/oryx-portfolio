cee220459e0224a12e938921da068b6fdb80a03f
oryxwhite <93627129+oryxwhite@users.noreply.github.com>
2022-01-15 20:27:40 -0500

    added order summary page

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-// homepage view at directory http://localhost:8080/
+// Landing page view at http://localhost:8080/
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/views/index.html'));
 });
 
 app.listen(process.env.PORT || 8080)
 
-// Stat preview card view
+// Project views
 app.use("/statcard", express.static(__dirname + '/views/statcard'));
+app.use("/ordersum", express.static(__dirname + '/views/ordersum'));
